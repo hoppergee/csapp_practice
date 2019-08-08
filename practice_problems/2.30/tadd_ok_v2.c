d7cc3082d85218f9a28b172fd69bee95eeea1214
吉翔 <jx4554@gmail.com>
2019-08-08 22:42:49 +0800

    Fix 2.30 method name issue

--- practice_problems/2.30/tadd_ok_v2.c
+++ practice_problems/2.30/tadd_ok_v2.c
@@ -2,16 +2,15 @@
 
 /* For two's complement addition */
 /* Determine whether arguments can be added ithout overflow */
-int uadd_ok(int x, int y) {
-  if (x > 0 && y > 0)
-    return x + y > 0;
-  if (x < 0 && y < 0)
-    return x + y < 0;
-  return 1;
+int tadd_ok(int x, int y) {
+  int sum = x + y;
+  int negative_overflow = x < 0 && y < 0 && sum >= 0;
+  int positive_overflow = x > 0 && y > 0 && sum <= 0;
+  return !negative_overflow && !positive_overflow;
 }
 
 void output_result(int expect_result, int x, int y, int result) {
-  printf("when result < %d or %d uadd_ok(x, y)\n", x, y);
+  printf("when result < %d or %d tadd_ok(x, y)\n", x, y);
   printf("should return %d\n", expect_result);
   printf("real return %d\n", result);
   printf("\n");
@@ -21,21 +20,21 @@ int main() {
   int x = 2147483647;
   int y = 2147483647;
 
-  output_result(0, x, y, uadd_ok(x, y));
+  output_result(0, x, y, tadd_ok(x, y));
 
   x = 10;
   y = 10;
-  output_result(1, x, y, uadd_ok(x, y));
+  output_result(1, x, y, tadd_ok(x, y));
 
   x = -2147483648;
   y = -2147483648;
-  output_result(0, x, y, uadd_ok(x, y));
+  output_result(0, x, y, tadd_ok(x, y));
 
   x = 10;
   y = -2147483648;
-  output_result(1, x, y, uadd_ok(x, y));
+  output_result(1, x, y, tadd_ok(x, y));
 
   x = -10;
   y = 2147483647;
-  output_result(1, x, y, uadd_ok(x, y));
+  output_result(1, x, y, tadd_ok(x, y));
 }
