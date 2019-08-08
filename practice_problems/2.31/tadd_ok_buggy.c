d1875080fa5cedac88becd7d0fb6beee2167723c
吉翔 <jx4554@gmail.com>
2019-08-08 22:20:30 +0800

    Rename uadd_ok to tadd_ok in 2.30 and 2.31

--- practice_problems/2.31/tadd_ok_buggy.c
+++ practice_problems/2.31/tadd_ok_buggy.c
@@ -2,13 +2,13 @@
 
 /* For two's complement addition */
 /* Determine whether arguments can be added ithout overflow */
-int uadd_ok(int x, int y) {
+int tadd_ok(int x, int y) {
   int sum = x + y;
   return (sum-x == y) && (sum-y == x);
 }
 
 void output_result(int expect_result, int x, int y, int result) {
-  printf("when result < %d or %d uadd_ok(x, y)\n", x, y);
+  printf("when result < %d or %d tadd_ok(x, y)\n", x, y);
   printf("should return %d\n", expect_result);
   printf("real return %d\n", result);
   printf("\n");
@@ -17,7 +17,7 @@ void output_result(int expect_result, int x, int y, int result) {
 int main() {
   int x = 2147483647;
   int y = 2147483647;
-  output_result(0, x, y, uadd_ok(x, y));
+  output_result(0, x, y, tadd_ok(x, y));
 
   int sum = x + y;
   printf("sum = 2147483647 + 2147483647 = %d\n", sum);
@@ -27,11 +27,11 @@ int main() {
 
   x = 10;
   y = 10;
-  output_result(1, x, y, uadd_ok(x, y));
+  output_result(1, x, y, tadd_ok(x, y));
 
   x = -2147483648;
   y = -2147483648;
-  output_result(0, x, y, uadd_ok(x, y));
+  output_result(0, x, y, tadd_ok(x, y));
 
   sum = x + y;
   printf("sum = -2147483648 + -2147483648 = %d\n", sum);
@@ -41,9 +41,9 @@ int main() {
 
   x = 10;
   y = -2147483648;
-  output_result(1, x, y, uadd_ok(x, y));
+  output_result(1, x, y, tadd_ok(x, y));
 
   x = -10;
   y = 2147483647;
-  output_result(1, x, y, uadd_ok(x, y));
+  output_result(1, x, y, tadd_ok(x, y));
 }
